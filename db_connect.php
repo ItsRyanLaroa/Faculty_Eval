0eb2b7b8abe11387108cc11d448b563f07874115
Ryan Ky Laroa <125838484+ItsRyanLaroa@users.noreply.github.com>
2024-11-04 21:06:44 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,13 @@
 <?php
-// Fetch database credentials from environment variables
 $host = getenv('DB_HOST');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
+$dbname = getenv('DB_DATABASE');
 
-// Connect to the database
-$conn = new mysqli($host, $username, $password, $database) or die("Could not connect to MySQL: " . mysqli_error($conn));
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
